24a9e5734c3350537d7c717da40e7ad05fddc5ad
Jim Ochieng <jimochieng64@gmail.com>
2026-03-02 00:24:00 +0300

    Updated code to pass some tests

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -40,7 +40,7 @@ class CashRegister:
   def apply_discount(self):
     discount = self.total * self._discount/100
     self.total -= discount
-    print(f"After the discount, the total comes to ${int(self.total)}")
+    print(f"After the discount, the total comes to ${int(self.total)}.")
       
     if not self.previous_transactions:
       print("There is no discount to apply.")
