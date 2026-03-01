08eb6f5748fefa8d3d0a3ad693d617c2de8dc42e
Jim Ochieng <jimochieng64@gmail.com>
2026-03-02 01:11:15 +0300

    Updated code to pass some tests

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -57,7 +57,8 @@ class CashRegister:
     
     # Handles subtracting the price of the removed item from the 'total'
     last_item = self.previous_transactions[-1]
-    self.total = self.total - (last_item['price'] * last_item['quantity'])
+    if type(last_item['quantity']) == int:
+      self.total = self.total - (last_item['price'] * last_item['quantity'])
 
     # Removes the last item in the 'previous_transactions' list
     self.previous_transactions.pop()
@@ -67,7 +68,7 @@ class CashRegister:
     
     
 c1 = CashRegister()
-c1.add_item('tomato', 1.76, 2)
+c1.add_item('tomato', 1.76)
 print(c1.items)
 print(c1.previous_transactions)
 print(c1.total) 
